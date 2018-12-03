442aa8954bf468063ab24654dd4087b38fdfa74a
middletone <middletone@science.regn.net>
2018-12-03 19:51:08 +0000

    Test and input for day 3 part 1

--- src/day3/main.ts
+++ src/day3/main.ts
@@ -1,4 +1,24 @@
-export function part1(_input: string[]): any {
+type Rectangle = {
+    left: number,
+    top: number,
+    width: number,
+    height: number
+}
+
+const INPUT_PATTERN: RegExp = new RegExp('#[0-9]+ @ (0-9)+,(0-9)+: (0-9)+x(0-9)+');
+
+function parseLine(line: string): Rectangle {
+    const matched: RegExpExecArray = INPUT_PATTERN.exec(line);
+    return {
+        left: matched[1],
+        top: matched[2],
+        width: matched[3],
+        height: matched[4]
+    }
+}
+
+export function part1(lines: string[]): any {
+    const rectangles: Rectangle[] = lines.map(parseLine);
     return 0;
 }
 
